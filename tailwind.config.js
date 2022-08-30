d56283066e8a784414aea1ba211bc65208b5197a
ganeshlohar10 <ganesh.lohar@xecurify.com>
2022-08-30 12:29:20 +0530

    Setup tailwind and base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                header: "75px",
+            },
+            gridTemplateColumns: {
+                primary: "repeat(auto-fit,minmax(198px,1fr))",
+                categories: "repeat(auto-fit,minmax(128px,1fr))",
+            },
+
+            keyframes: {
+                pop: {
+                    "0%": { transform: "scale(1)" },
+                    "50%": { transform: "scale(0.95)" },
+                    "100%": { transform: "scale(1)" },
+                },
+            },
+            animation: {
+                pop: "pop 0.2s ease-in",
+            },
+        },
+    },
+    plugins: [],
 };
