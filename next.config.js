6bbc473728d5c5769b50f510205a71bb1afb5528
ganeshlohar10 <ganesh.lohar@xecurify.com>
2022-09-08 12:44:38 +0530

    :wrench:

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
-    trailingSlash: true,
+    // trailingSlash: true,
     /*
         Todo
             For dev: Comment [ basePath: "/blog", ]
             For build: Uncomment [ basePath: "/blog", ]
      */
     basePath: "/blog",
+    images: {
+        loader: "imgix",
+        path: "/blog",
+    },
 };
 
 module.exports = nextConfig;
