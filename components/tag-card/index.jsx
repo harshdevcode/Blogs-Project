843dffb0851cb1b179df9f7d0cf2145f9ac56f7e
ganeshlohar10 <ganesh.lohar@xecurify.com>
2022-11-29 18:01:27 +0530

    :art: Minor UI Changes and Font Change to DM Sans

--- components/tag-card/index.jsx
+++ components/tag-card/index.jsx
@@ -4,7 +4,7 @@ function TagCard({ tag }) {
     return (
         <div
             className={`
-                [ py-3 px-4 rounded-md bg-white flex flex-row gap-4 items-center justify-center font-medium border cursor-pointer duration-150 ]
+                [ py-2 px-4 rounded-md bg-white flex flex-row gap-4 items-center justify-center font-medium border cursor-pointer duration-150 ]
                 [ hover:bg-slate-100 ]
             `}
         >
