38e61252168ae14c38cf267aa70cfa972496ddef
ganeshlohar10 <ganesh.lohar@xecurify.com>
2022-11-29 18:00:58 +0530

    Created Comment Feature with ISR support for comments

--- components/comment-box/index.jsx
+++ components/comment-box/index.jsx
@@ -1,14 +1,45 @@
 import React from "react";
+import { POSTING_COMMENT } from "../../utils/string-constants";
+import Spinner from "../spinner";
 
-function CommentBox() {
+function CommentBox({ value, onCommentChange, onPostComment, status }) {
     return (
-        <div className="flex flex-col gap-6 border p-6 rounded-smooth">
-            <textarea
-                placeholder="Write your comment here"
-                className="border-none outline-none resize-none h-18"
+        <div className="flex flex-col border rounded-smooth divide-y overflow-hidden bg-white">
+            <input
+                disabled={status === POSTING_COMMENT}
+                value={value.user_name}
+                onChange={onCommentChange}
+                type="text"
+                name="user_name"
+                className="outline-none p-4"
+                placeholder="Full Name"
             />
-            <button className="button primary w-max self-end">
-                Post Comment
+            <input
+                disabled={status === POSTING_COMMENT}
+                value={value.user_email}
+                onChange={onCommentChange}
+                type="email"
+                name="user_email"
+                className="outline-none p-4"
+                placeholder="Email Address"
+            />
+            <textarea
+                disabled={status === POSTING_COMMENT}
+                value={value.content}
+                onChange={onCommentChange}
+                name="content"
+                placeholder="Write your comment here"
+                className="outline-none resize-none h-28 p-4"
+            />
+            <button
+                disabled={status === POSTING_COMMENT}
+                onClick={onPostComment}
+                className="button primary w-max self-end m-4"
+            >
+                {status === POSTING_COMMENT && <Spinner />}
+                <span>
+                    {status === POSTING_COMMENT ? "Posting" : "Post Comment"}
+                </span>
             </button>
         </div>
     );
--- components/comment-card/index.jsx
+++ components/comment-card/index.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-function CommentCard() {
+function CommentCard({ user, content }) {
     return (
         <li className="flex p-6 gap-4">
             <div className="w-14 h-14 bg-slate-100 rounded-full"></div>
             <section className="mt-1 flex-1">
-                <p className="title">Ganesh Lohar</p>
-                <p className="body">
-                    all the work people, fulfilling their function! super cool
-                    illustration!
-                </p>
+                <p className="title">{user.name}</p>
+                <p className="body">{content}</p>
             </section>
         </li>
     );
