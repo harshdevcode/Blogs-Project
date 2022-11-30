b1f8313b54b34a009fde0971945bc8109753e92e
ganeshlohar10 <ganesh.lohar@xecurify.com>
2022-11-30 11:31:02 +0530

    Added theme values to tag card classes

--- components/tag-card/index.jsx
+++ components/tag-card/index.jsx
@@ -4,8 +4,8 @@ function TagCard({ tag }) {
     return (
         <div
             className={`
-                [ py-2 px-4 rounded-md bg-white flex flex-row gap-4 items-center justify-center font-medium border cursor-pointer duration-150 ]
-                [ hover:bg-slate-100 ]
+                [ py-small px-regular rounded-md bg-primary-bg flex flex-row gap-4 items-center justify-center font-medium border border-primary-border cursor-pointer duration-150 ]
+                [ hover:bg-secondary-bg ]
             `}
         >
             {tag}
