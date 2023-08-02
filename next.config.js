c59453588fce561423521aa799e7927f149d7caf
Rohan_Nikam <rohan@xecurify.com>
2023-08-02 12:12:56 +0530

    Added basePath in next.config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
             For dev: Comment [ basePath: "/blog", ]
             For build: Uncomment [ basePath: "/blog", ]
      */
-
+    basePath: "/blog",
     images: {
         domains: [
             'testsite.miniorange.com',
