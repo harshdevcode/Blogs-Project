c62cfef2d681caabb69aaa0ca23c8537e42ff3a3
ganeshlohar10 <ganesh.lohar@xecurify.com>
2023-12-06 13:27:22 +0530

    Comments loading fix and changed cc email to doc-team

--- components/comment-card/index.jsx
+++ components/comment-card/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function CommentCard({ user, content }) {
     return (
-        <li className='flex p-rg gap-rg'>
-            <div className='w-14 h-14 bg-slate-100 rounded-full'></div>
+        <li className='flex p-rg gap-rg bg-primary rounded border'>
+            <div className='w-14 h-14 bg-slate-200 rounded-full'></div>
             <section className='mt-xs flex-1'>
                 <p className='title'>{user.name}</p>
                 <p className='body'>{content}</p>
