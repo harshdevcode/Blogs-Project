a75db6ffb862e5a2b496cf978e8a4fcecf3229ae
Abhishek Jadhav <abhishek.jadhav@xecurify.com>
2024-01-25 11:19:32 +0530

    Removing NEXT_PUBLIC prefix from api calll

--- pages/api/search/index.js
+++ pages/api/search/index.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${term}&start=${start}`
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.SEARCH_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=${term}&start=${start}`
     );
     const data = await response.json();
     res.status(200).json(data);
