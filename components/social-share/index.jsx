c4ea1720416e674b00ba5e912bc8a27e44616ac1
Saniya ALi <saniya.ali@xecurify.com>
2024-05-13 15:37:14 +0530

    Share icon UI changes

--- components/social-share/index.jsx
+++ components/social-share/index.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
 	FacebookShareButton,
 	FacebookIcon,
-	PinterestShareButton,
-	PinterestIcon,
-	RedditShareButton,
-	RedditIcon,
 	WhatsappShareButton,
 	WhatsappIcon,
 	LinkedinShareButton,
@@ -18,92 +14,60 @@ import {
 import { Share2 } from 'lucide-react';
 import styles from 'styles/components/social-share.module.css';
 
-
 const SocialShare = ({ url, title }) => {
 	const [shareOpen, setShareOpen] = useState(false);
 	const [clicked, setClicked] = useState(false);
 
 	const toggleShareOpen = () => {
 		setShareOpen(!shareOpen);
-
-		setClicked(true); // Set clicked to true when icon is clicked
+		setClicked(true)
 		setTimeout(() => {
-			setClicked(false); // Remove clicked class after a short delay
-		}, 300);
+			setClicked(false)
+		}, 200)
 	};
+
+	const handleCloseOutsideClick = (e) => {
+		const shareContainer = document.getElementById('shareContainer');
+		const shareIcon = document.getElementById('shareIcon');
+		if (shareContainer && !shareContainer.contains(e.target) && !shareIcon.contains(e.target)) {
+			setShareOpen(false); // Close the share tray if clicked outside and not on the share icon
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleCloseOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleCloseOutsideClick);
+		};
+	}, []);
+
 	return (
 		<div className="fixed cursor-pointer">
-
-			<div className={`${styles.shareContainer} ${shareOpen ? styles.open : ''}`} >
-				<FacebookShareButton
-
-					url={url}
-					windowWidth={700}
-					windowHeight={700}
-					blankTarget={true}
-				>
+			<div id="shareContainer" className={`${styles.shareContainer} ${shareOpen ? styles.open : ''}`}>
+				<FacebookShareButton url={url} title={title} windowWidth={700} windowHeight={700} blankTarget={true}>
 					<FacebookIcon size={32} round className={styles.icons} />
 				</FacebookShareButton>
-				{/* <PinterestShareButton
-						windowWidth={700}
-						windowHeight={700}
-						url={url}
-					>
-						<PinterestIcon size={32} round />
-					</PinterestShareButton> */}
-				{/* <RedditShareButton
-						url={url}
-						windowWidth={700}
-						windowHeight={700}
-					>
-						<RedditIcon size={32} round />
-					</RedditShareButton> */}
-				<WhatsappShareButton
-					url={url}
-					windowWidth={700}
-					windowHeight={700}
-					blankTarget={true}
-				>
+				<WhatsappShareButton url={url} windowWidth={700} windowHeight={700} blankTarget={true}>
 					<WhatsappIcon size={32} round className={styles.icons} />
 				</WhatsappShareButton>
-				<LinkedinShareButton
-					url={url}
-					windowWidth={700}
-					windowHeight={700}
-					blankTarget={true}
-				>
+				<LinkedinShareButton url={url} windowWidth={700} windowHeight={700} blankTarget={true}>
 					<LinkedinIcon size={32} round className={styles.icons} />
 				</LinkedinShareButton>
-				<TwitterShareButton
-					url={url}
-					windowWidth={700}
-					windowHeight={700}
-					blankTarget={true}
-				>
+				<TwitterShareButton url={url} windowWidth={700} windowHeight={700} blankTarget={true}>
 					<TwitterIcon size={32} round className={styles.icons} />
 				</TwitterShareButton>
-				<EmailShareButton
-					url={url}
-					windowWidth={700}
-					windowHeight={700}
-					blankTarget={true}
-				>
+				<EmailShareButton url={url} windowWidth={700} windowHeight={700} blankTarget={true}>
 					<EmailIcon size={32} round className={styles.icons} />
 				</EmailShareButton>
 			</div>
-
-			<div className={` fixed right-[22px] bottom-[90px]  ${clicked ? styles.clicked : ''}`}>
-				<button
-					className='pr-3 pl-2 py-2 bg-accent rounded-full '
-					onClick={() => toggleShareOpen()}
-				>
-					<Share2 size={32} color='white' strokeWidth={2} />
+			<div className={`fixed right-[22px] bottom-[90px] `} id="shareIcon"  onClick={toggleShareOpen}>
+				<button className={`pr-3 pl-2 py-2 rounded-full ${styles.shareBg} ${clicked ? styles.clicked : ''}`}>
+					<Share2 size={32} color='#EB5424' strokeWidth={2} />
 				</button>
 				<div className=' text-accent font-bold text-sm text-center'>Share</div>
 			</div>
 		</div>
-
 	)
 }
 
-export default SocialShare
+export default SocialShare;
