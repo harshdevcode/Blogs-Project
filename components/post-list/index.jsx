db43f18393621b462fba4065db49331de8833da2
Saniya ALi <saniya.ali@xecurify.com>
2024-05-16 16:12:28 +0530

    all posts sorted latest first

---
+++ components/post-list/index.jsx
@@ -0,0 +1,36 @@
+import Icon from 'components/lucide-icon';
+import { Subtitle, Title } from 'components/typography';
+import { capitalizeFirstLetter } from 'helpers/string';
+import Link from 'next/link';
+
+function PostList({ post, index }) {
+	return (
+		<Link
+			key={index}
+			href={`/${post?.slug}`}
+			className='card flex flex-col duration-150 cursor-pointer mt-4 '
+		>
+			<div className="py-4 px-8">
+				<Title weight="semibold" className="line-clamp-2">
+					{post.title}
+				</Title>
+				<Subtitle className={`mt-xs caption truncate`}>{post.description}</Subtitle>
+				<div className="flex items-center gap-2.5 mt-5">
+					<Icon name="Clock" color="grey" size={18} />
+					<Subtitle className="grow">{new Date(post.createdOn).toDateString()}</Subtitle>
+					<Icon name="FolderClosed" color="grey" size={18} />
+					<div className="flex flex-wrap items-center">
+						{post.category.map((_category, i) => (
+							<>
+								<Subtitle>{capitalizeFirstLetter(_category)}</Subtitle>
+								{i !== post.category.length - 1 ? <>, &nbsp;</> : null}
+							</>
+						))}
+					</div>
+				</div>
+			</div>
+		</Link>
+	)
+}
+
+export default PostList
--- pages/allBlogs/index.js
+++ pages/allBlogs/index.js
@@ -1,25 +1,44 @@
 "use-strict"
+import { useState } from 'react';
 import { getAllPosts } from '../../helpers/helpers';
-import PostItem from '../../components/post-item';
-import Headline from 'components/heading';
 import styles from 'styles/homepage.module.css';
+import Headline from 'components/heading';
+import PostList from 'components/post-list';
+
 
 export default function AllPosts({ posts }) {
+    const [sortingCriteria, setSortingCriteria] = useState('latest'); // Default sorting criteria
+
+    // Function to handle sorting criteria change
+    const handleSortingCriteriaChange = (criteria) => {
+        setSortingCriteria(criteria);
+    };
+
+    // Sort posts based on the selected sorting criteria
+    const sortedPosts = posts.sort((a, b) => {
+        if (sortingCriteria === 'latest') {
+            return new Date(b.createdOn) - new Date(a.createdOn); // Sort by latest first
+        } 
+        // else if (sortingCriteria === 'category') {
+        //     return a.category.localeCompare(b.category); // Sort by category
+        // }
+    });
+
     return (
         <div className={styles.homepage_container}>
-             <Headline text="All Posts" />
-            <div className="latest-posts-grid"> 
-                        {posts.map((post, index) => (
-                            <PostItem {...post} key={post.slug} />
-                        ))}
-                    </div>
+            {/* <button onClick={() => handleSortingCriteriaChange('latest')}>Sort by Latest</button>
+            <button onClick={() => handleSortingCriteriaChange('category')}>Sort by Category</button> */}
+
+            <Headline text="All Posts" />
+            {sortedPosts.map((post, index) => (
+                <PostList index={index} post={post} />
+            ))}
         </div>
     );
 }
 
 export async function getStaticProps() {
-    console.log('fetched')
-    const posts = getAllPosts(['title', 'description', 'createdOn', 'slug', 'thumbnail']);
+    const posts = getAllPosts(['title', 'description', 'createdOn', 'slug', 'category']);
     return {
         props: {
             posts,
