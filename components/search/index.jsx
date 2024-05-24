d2fb68177cf73789f904071d3dc904f4b6c13ef1
Saniya ALi <saniya.ali@xecurify.com>
2024-05-24 13:18:21 +0530

    Added pagination and improved filtering

--- components/search/index.jsx
+++ components/search/index.jsx
@@ -1,43 +1,71 @@
-import React, {useState, useRef} from 'react';
+import {React, useState, useRef, useEffect} from 'react';
 import SearchResults from 'components/searchresults';
+import PaginationButtons from 'components/pagination-buttons';
 import RecommendedSearches from 'components/search/recommendedSearches.jsx';
-import Spinner from 'components/spinner';
+import LoadingStatus from 'components/search/loadingStatus.jsx';
 import Icon from 'components/lucide-icon';
 import Button from 'components/button';
-import useFetch from 'hooks/useFetch';
 
 export default function Search({ closeSearchDialog }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fetchedData, setFetchedData] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(false);
+  const [nonemptyResult, setNonemptyResult] = useState(false);
+  const [error, setError] = useState(null);
   const [activePage, setActivePage] = useState(1);
   const inputRef = useRef(null);
-  const {loading, data: fetchedData, error, start} = useFetch();
 
+  
+  useEffect(() => {
+    if (searchTerm) {
+      fetchData(searchTerm, activePage);
+    }
+  }, [searchTerm, activePage]);
+
+  
   const handleChange = (event) => {
     if (event.type === 'click' || event.key === 'Enter') {
-      fetchResults(event.target.value, activePage);
+      fetchData(inputRef.current.value, activePage);
+      setSearchTerm(inputRef.current.value);
     }
-    setSearchTerm(event.target.value);
   };
-
-  const onSearch = () => {
-    fetchResults(searchTerm, activePage);
-  }
-
-  const fetchResults = (_searchTerm, _activePage) => {
-    start('/blog/api/search', 'get', null, null, `term=${_searchTerm}&start=${_activePage}`);
-  }
-
+  
   const navigationPageChange = (newPage) => {
-    fetchResults(searchTerm, newPage);
+    fetchData(searchTerm, newPage);
     setActivePage(newPage);
   };
-
+  
   const searchedRecommended = (searchedKey) => {
     inputRef.current.value = searchedKey;
-    fetchResults(searchedKey, 1);
+    fetchData(searchedKey, 1);
     setSearchTerm(searchedKey);
   };
 
+  const fetchData = async (searchTerm, start) => {
+    setLoading(true);
+    setError(null);
+    try {
+
+      const response = await fetch('/blog/api/search?term=' + searchTerm + '&start=' + (start - 1) * 10);
+      const key = await response.json();
+      // console.log('fetched data', key);
+      if(key.data.searchInformation.totalResults === "0"){
+        console.log("inside key.searchInformation.totalResults === 0");
+        setNonemptyResult(false);
+      }
+      else{
+        setNonemptyResult(true);
+        console.log("False is key.searchInformation.totalResults === 0");
+      }
+      setFetchedData(key);
+    } catch (error) {
+      setError(error.message || "An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  const totalPages = Math.ceil(fetchedData.data?.searchInformation.totalResults / 10);
   return (
     <div className="relative z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
@@ -46,27 +74,25 @@ export default function Search({ closeSearchDialog }) {
           <div className="flex flex-col relative rounded-lg bg-white shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-4xl" style={{minHeight: "50vh"}}>
             <div className="modal-heading flex justify-between items-center border-b-2 p-5 sm:p-8">
               <div className='flex w-full'>
-                <input type='text' onKeyDown={handleChange} onChange={handleChange} value={searchTerm} placeholder='Enter search term' ref={inputRef} className='input w-9/12 mr-2 text-sky-600 shadow-sm placeholder:text-sky-600 focus:outline-none bg-blue-50 focus:bg-blue-50 border-none ring-1 ring-inset ring-sky-500/10'></input>
-                <Button text="Search" onClick={onSearch} variant="secondary" className="shadow"/>
+                <input type='text' onKeyDown={handleChange} placeholder='Enter search term' ref={inputRef} className='input w-9/12 mr-2 text-sky-600 shadow-sm placeholder:text-sky-600 focus:outline-none bg-blue-50 focus:bg-blue-50 border-none ring-1 ring-inset ring-sky-500/10'></input>
+                <Button text="Search" onClick={handleChange} variant="secondary" className="shadow"/>
               </div>
               <div className="modal-btncontainer flex items-center">
                 <button onClick={closeSearchDialog}><Icon name="X" color="#EB5424" size={24}/></button>
               </div>
             </div>
-            <div className="modal-body sm:flex grow bg-white border-b-2 w-full">
-              <div className="modal-content border-t-2 sm:border-t-0 w-full">
-                {loading ? (
-                  <div className='flex justify-center items-center h-full'>
-                    <Spinner variant='secondary' size="w-12"/>
-                  </div>) : (
-                  <>
-                    { fetchedData ? <SearchResults returnedData={fetchedData} activePage={activePage} navigationPageChange={navigationPageChange}/> : (
-                      <RecommendedSearches searchedRecommended={searchedRecommended}/>
-                    ) }
-                  </>
-                )}
+            <div className="modal-body sm:flex grow bg-white border-b-2">
+              <div className="modal-content border-t-2 sm:border-t-0 p-5 sm:p-8 h-96 w-full overflow-auto">
+                  {loading ? (<LoadingStatus/>) : (
+                    <div className='results-div h-full'>
+                      { fetchedData ? <SearchResults returnedData={fetchedData.data}/> : (
+                        <RecommendedSearches searchedRecommended={searchedRecommended}/>
+                      ) }
+                    </div>
+                  )}
               </div>
             </div>
+            {nonemptyResult && <PaginationButtons totalPages={totalPages}  currentPage={activePage} onPageChange={navigationPageChange}/>}
           </div>
         </div>
       </div>
